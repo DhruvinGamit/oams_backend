13deb09a1f53e571489724c07e20e324c2e8bc90
DhruvinGamit <dhruving21@gmail.com>
2024-01-11 19:30:48 +0530

    commit

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,6 @@
-// categoryRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
-// const { isAdmin } = require('../middleware/authMiddleware');
 
 router.post('/add', categoryController.addCategory);
 
