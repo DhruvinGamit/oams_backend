03c4117f27d6d38c5129c9d1fbe4f07da0159b6b
DhruvinGamit <dhruving21@gmail.com>
2024-05-04 10:00:31 +0530

    commit

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://127.0.0.1/OAMS3',
+      process.env.MONGO_URI || 'mongodb://127.0.0.1/OAMS2',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
